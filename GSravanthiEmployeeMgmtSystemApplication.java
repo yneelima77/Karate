16ef8be25ec95f7f8a045b9988d491ae1a26d820
yneelima77 <35612917+yneelima77@users.noreply.github.com>
2023-07-27 12:27:03 -0400

    Add files via upload
    default url: http://localhost:8080/login

---
+++ GSravanthiEmployeeMgmtSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.G_Sravanthi_EmployeeMgmtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+
+public class GSravanthiEmployeeMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GSravanthiEmployeeMgmtSystemApplication.class, args);
+	}
+
+}
